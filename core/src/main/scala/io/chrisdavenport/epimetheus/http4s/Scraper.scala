64e83287aa0b7cd3cb7bed08b27dad4fa2908717
Arthur Sengileyev <arthur.sengileyev@gmail.com>
2020-04-10 22:37:54 +0300

    Support for using collection of registries in Scraper

--- core/src/main/scala/io/chrisdavenport/epimetheus/http4s/Scraper.scala
+++ core/src/main/scala/io/chrisdavenport/epimetheus/http4s/Scraper.scala
@@ -6,16 +6,63 @@ import cats.effect._
 import org.http4s._
 import org.http4s.dsl.Http4sDsl
 import io.chrisdavenport.epimetheus.CollectorRegistry
+import io.prometheus.client.exporter.common.TextFormat
+import org.http4s.headers.`Content-Type`
 
 object Scraper {
 
-  def response[F[_]: Functor](cr: CollectorRegistry[F]): F[Response[F]] = 
-    cr.write004.map(Response[F](Status.Ok).withEntity(_))
+  lazy val prometheusContentType: `Content-Type` = `Content-Type`
+    .parse(TextFormat.CONTENT_TYPE_004)
+    .leftMap(_ => `Content-Type`(MediaType.text.plain))
+    .merge
 
-  def routes[F[_]: Sync](cr: CollectorRegistry[F]): HttpRoutes[F] = {
+  private def makeResponse[F[_], T](
+    b: T
+  )(implicit w: EntityEncoder[F, T]
+  ) =
+    Response[F](Status.Ok).withEntity(b).withContentType(prometheusContentType)
+
+  def response[F[_]: Functor](
+    cr: CollectorRegistry[F]
+  ): F[Response[F]] =
+    cr.write004.map(makeResponse(_))
+
+  def response[F[_]: Applicative, T[_]: Foldable](
+    c: T[CollectorRegistry[F]]
+  ): F[Response[F]] =
+    c.foldMapA(_.write004).map(makeResponse(_))
+
+  def responsePar[F[_]: Applicative, T[_]: Foldable](
+    c: T[CollectorRegistry[F]]
+  )(implicit p: Parallel[F]
+  ): F[Response[F]] =
+    c.parFoldMapA(_.write004).map(makeResponse(_))
+
+  def routes[F[_]: Sync](
+    cr: CollectorRegistry[F]
+  ): HttpRoutes[F] = {
     val dsl = new Http4sDsl[F]{}; import dsl._
     HttpRoutes.of[F] {
       case GET -> Root / "metrics" => response(cr)
+    }
+  }
+
+  def routes[F[_]: Sync, T[_]: Foldable](
+    c: T[CollectorRegistry[F]]
+  ): HttpRoutes[F] = {
+    val dsl = new Http4sDsl[F]{}; import dsl._
+    HttpRoutes.of[F] {
+      case GET -> Root / "metrics" => response(c)
+    }
+  }
+
+  def routesPar[F[_]: Sync, T[_]: Foldable](
+    c: T[CollectorRegistry[F]]
+  )(implicit p: Parallel[F]
+  ): HttpRoutes[F] = {
+    val dsl = new Http4sDsl[F]{}; import dsl._
+    HttpRoutes.of[F] {
+      case GET -> Root / "metrics" => responsePar(c)
     }
   }
 }
